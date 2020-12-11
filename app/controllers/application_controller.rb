9fbce890e5a7f12d3f27c2c6973e2ad3760c919d
Lina Vallejo <linav92@gmail.com>
2020-12-11 10:17:39 -0300

    Restringir vistas usuarios no autenticados

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+  def authenticate_user!
+    unless @current_user.present?
+      flash[:danger] = "No tienes autorización para entrar en esa sección"
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,7 @@
 class StoriesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_story, only: [:show, :edit, :update, :destroy]
+
 
   # GET /stories
   # GET /stories.json
